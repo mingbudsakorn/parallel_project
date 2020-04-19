c7e33a29fdf57ecd6ff7f985b641f37b52da2339
Jinwara <naon123@hotmail.com>
2020-04-19 15:33:49 +0700

    add new component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-import Main from './pages/Main'
-import Login from './pages/Login'
-
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Test from './pages/Test';
 
 const App = () => {
-  const [uid, setUid] = useState(-1)
-  const [gid, setGid] = useState(-1)
+  const [uid, setUid] = useState(-1);
+  const [gid, setGid] = useState(-1);
   // useState
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       {window.location.pathname === '/login' && <Login changeUid={setUid} />}
-      {window.location.pathname === '/main' && <Main uid={uid} gid={gid} changeGid={setGid} />}
+      {window.location.pathname === '/main' && (
+        <Main uid={uid} gid={gid} changeGid={setGid} />
+      )}
+      {window.location.pathname === '/test' && (
+        <Test uid={uid} gid={gid} changeGid={setGid} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ChatBox from '../components/ChatBox';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Test</div>
+        <ChatBox />
+      </div>
+    );
+  }
+}
+
+export default Test;
