440fa5b1d8d286023a2164d730db8e73b5d40f65
Jinwara <naon123@hotmail.com>
2020-04-20 23:48:30 +0700

    main2 almost complete; arrangement is a bit weird

--- frontend/src/pages/Main2.js
+++ frontend/src/pages/Main2.js
@@ -3,22 +3,22 @@ import Bar from '../components/Bar';
 import { Paper } from '@material-ui/core/';
 import TextField from '@material-ui/core/TextField';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ChatBox from '../components/ChatBox';
 
 const Main = () => {
   return (
     <div>
       <Bar />
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', height: '92vh' }}>
         <Paper
           square
           elevation={0}
+          width="40vh"
           style={{
             display: 'flex',
             justifyContent: 'space-between',
             flexDirection: 'column',
-            backgroundColor: '#424141',
-            width: '40vh',
-            height: '80vh',
+            backgroundColor: '#424242',
             padding: '20px 15px 30px 15px',
           }}
         >
@@ -49,6 +49,32 @@ const Main = () => {
               style={{ color: '#FFF100', fontSize: 36, marginLeft: 6 }}
             />
           </div>
+        </Paper>
+        <Paper
+          square
+          elevation={0}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexDirection: 'column',
+            padding: '20px 20px 30px 20px',
+            width: '126vh',
+          }}
+        >
+          <div>
+            <ChatBox />
+          </div>
+          <TextField
+            variant="outlined"
+            placeholder="Message.."
+            size="small"
+            style={{
+              backgroundColor: '#EFEFEF',
+              width: '100%',
+              borderRadius: 6,
+              marginLeft: 4,
+            }}
+          />
         </Paper>
       </div>
     </div>
