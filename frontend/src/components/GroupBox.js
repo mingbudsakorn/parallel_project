1aa868a0441606b00e1b4c92834844ff8df0e5a7
Jinwara <naon123@hotmail.com>
2020-04-21 19:32:04 +0700

    add unread sign

--- frontend/src/components/GroupBox.js
+++ frontend/src/components/GroupBox.js
@@ -1,40 +1,50 @@
 import React from 'react';
 
-
 const GrouupBox = () => {
-    
-    //status 
-    const [status1, setStatus1] = React.useState(false);
-    const [status2, setStatus2] = React.useState(true);
-    const [status3, setStatus3] = React.useState(true);
+  //status
+  const [status1, setStatus1] = React.useState(false);
+  const [status2, setStatus2] = React.useState(true);
+  const [status3, setStatus3] = React.useState(true);
 
-        return(
-            <div style={{display:'flex', justifyContent:'space-between'}}>
-                {!status1 && (
-                <div style={{backgroundColor: status3 ? '#606060':'none',width:'40vh',display:'flex', justifyContent:'space-between'}}>
-                    <div style={{color:'#EFEFEF', fontSize:'20px', marginLeft:35, marginTop:15}}>
-                        GROUP1
-                    </div>
-                    {status2 && (
-                <div style={{color:'#EFEFEF', fontSize:'20px', marginLeft:35, marginTop:15}}>
-                    unread
-                </div>
-                
-                )
-                }
-                </div>
-
-                )
-                }
-               
-
-            </div>
-            
-            
-        )
-    
-    
-}
-
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      {!status1 && (
+        <div
+          style={{
+            backgroundColor: status3 ? '#606060' : 'none',
+            width: '44vh',
+            height: '56px',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
+            style={{
+              color: '#EFEFEF',
+              fontSize: '20px',
+              marginLeft: 35,
+              marginTop: 15,
+            }}
+          >
+            GROUP1
+          </div>
+          {status2 && (
+            <div
+              style={{
+                display: 'flex',
+                alignSelf: 'center',
+                marginRight: 35,
+                borderRadius: '100%',
+                backgroundColor: '#FF8258',
+                height: '12px',
+                width: '12px',
+              }}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default GrouupBox;
