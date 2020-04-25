dba691a587ab5aee6dbf74b6e9870c56e78fde3a
focusjk <jiraphat.khupanit@gmail.com>
2020-04-25 14:47:37 +0700

    ngrok

--- frontend/src/ip.js
+++ frontend/src/ip.js
@@ -1,6 +1,6 @@
 const ip = {
-  loadBalancer: 'http://127.0.0.1:5000',
-  socketServer: 'http://127.0.0.1:5001',
+  loadBalancer: 'https://e9b53594.ngrok.io',
+  socketServer: 'https://c1f95175.ngrok.io',
   primaryBackend: 'http://127.0.0.1:4000',
   secondaryBackend: 'http://127.0.0.1:4001',
   frontend: 'http://127.0.0.1:3000',
